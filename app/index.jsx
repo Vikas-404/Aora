617c2921903fe311638763eb7e04d6410f88b726
Vikas-404 <vikasassudani909@gmail.com>
2024-05-19 11:34:08 +0530

    Fully Implemented Bottom Tab Bar with proper styling and routing

--- app/index.jsx
+++ app/index.jsx
@@ -7,8 +7,8 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pextrabold">Home</Text>
       <StatusBar style="auto"/>
-      <Link href="/profile" style={{color: "blue"}}>
-      Go to Profile </Link>
+      <Link href="/home" style={{color: "blue"}}>
+      Go to Home </Link>
     </View>
   );
 }
