f76d87bce030b6eed8c69abfe741a5797bbcfa90
Vikas-404 <vikasassudani909@gmail.com>
2024-05-27 11:24:35 +0530

    Onboarding done perfectly

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Text, View, Image } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../constants";
@@ -13,7 +13,7 @@ export default function App() {
         {/* Added scrollview as it would be easier to scroll for smaller devices user as the content may get out of their screen and height 100% allows us to make the whole screen scrollable*/}
 
         {/* Logo and Card Image */}
-        <View className="w-full items-center h-full px-4">
+        <View className="w-full justify-center items-center h-full px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px}"
@@ -31,17 +31,27 @@ export default function App() {
               Discover Endless Possibilities with{" "}
               <Text className="text-secondary-200">Aora</Text>
             </Text>
-            <Image 
+            <Image
               source={images.path}
               className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
               resizeMode="contain"
-              />
+            />
           </View>
 
-          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: Embark on a journey of limitless exploration with Aora</Text>
-          <CustomButton></CustomButton>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: Embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          >
+            {" "}
+          </CustomButton>
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
